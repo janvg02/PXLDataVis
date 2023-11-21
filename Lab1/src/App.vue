156a0fc15659c23b1332184055f3760f9ddba5a3
janvg02 <jan.vangalen@student.pxl.be>
2023-11-21 16:05:11 +0100

    begin data-arrays

---
+++ Lab1/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import Face from "@/components/Face.vue";
+</script>
+
+<template>
+  <div id="app">
+    <svg width="700" height="350">
+      <g v-for="n in 3" :key="n">
+        <Face
+            :x="50 + (n-1) * 100"
+            y="150"
+            :stroke-width="3"
+            :eyeOffsetX="20"
+            :eyeOffsetY="10"
+            :eyeRadius="8"
+            :r="45"
+        />
+      </g>
+    </svg>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ Lab1/src/components/Face.vue
@@ -0,0 +1,40 @@
+<template>
+  <g :transform="`translate(${x}, ${y})`">
+    <circle :r="r" fill="yellow" stroke="black" :stroke-width="strokeWidth"></circle>
+    <circle :cx="-eyeOffsetX" :cy="-eyeOffsetY" :r="eyeRadius" fill="black"></circle>
+    <circle :cx="eyeOffsetX" :cy="-eyeOffsetY" :r="eyeRadius" fill="black"></circle>
+  </g>
+</template>
+
+<script setup>
+defineProps({
+  x: {
+    type: Number,
+    required: true
+  },
+  y: {
+    type: Number,
+    required: true
+  },
+  r: {
+    type: Number,
+    required: true
+  },
+  strokeWidth: {
+    type: Number,
+    required: true
+  },
+  eyeOffsetX: {
+    type: Number,
+    required: true
+  },
+  eyeOffsetY: {
+    type: Number,
+    required: true
+  },
+  eyeRadius: {
+    type: Number,
+    required: true
+  }
+})
+</script>
