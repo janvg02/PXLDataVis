80e4c6ebdf0eb918c7daa9a85c18feee08b1b70c
janvg02 <jan.vangalen@student.pxl.be>
2024-01-14 18:43:51 +0100

    probeer

---
+++ data-processing/data-processing/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  mounted() {
+    console.log('Hello')
+  }
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ data-processing/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  mounted() {
+    console.log('Hello')
+  }
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
