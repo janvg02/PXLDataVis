7f544ae6dee09061174c267196b534797cd58aa4
janvg02 <jan.vangalen@student.pxl.be>
2024-01-15 14:37:19 +0100

    h3 afgerond

--- data-processing/data-processing/src/App.vue
+++ data-processing/data-processing/src/App.vue
@@ -61,41 +61,98 @@ onMounted(async() => {
 })*/
 
 
-let movieData = ref([]);
-
-function typeConversion(d) {
+/* oef 2 oefeningen
+function typeConversion(d)
+{
   return {
-    genre: d.genre.toLowerCase(),
-    budget: +d.budget,
-    revenue: +d.revenue
+    title: d.title.toLowerCase(),
+    vote_average: +d.vote_average,
+    vote_count: +d.vote_count,
+    homepage: +d.homepage,
   };
-}
-function prepareBarChart(d){
-  const dataMap = d3.rollup(
-      d,
-      r => d3.sum(r, x => x.revenue),
-      d => d.genre
-  )
-  const dataArray = Array.from(dataMap, d => ({ genre: d[0], revenue: d[1] }));
-  return dataArray;
 }
 function dataLoaded(d){
   return d.filter ( r=> {
-   return r.budget > 0 && r.genre && r.revenue > 0
+    return r.title  && r.vote_average && r.vote_count > 1000 ;
   })
 }
 
 onMounted(async() => {
   const data = await d3.csv('movies.csv', typeConversion);
-  const filteredMovies = dataLoaded(data);
-  const finishedData = prepareBarChart(filteredMovies)
-  console.log(finishedData);
-})
+  const filteredMovies = dataLoaded(data)
+  console.log(filteredMovies);
+})*/
 
+/*Oef 3 oefeningen
+function typeConversion(d)
+{
+  const date = d3.timeParse('%Y-%m-%d');
+  const countries = JSON.parse(d.production_countries );
+  return {
+    title: d.title.toLowerCase(),
+    production_countries: countries,
+    release_date: +date(d.release_date)
+  };
+}
+function dataLoaded(d){
+  return d.filter ( r=> {
+    return r.title && r.production_countries && r.release_date ;
+  })
+}
+
+onMounted(async() => {
+  const data = await d3.csv('movies.csv', typeConversion);
+  const filteredMovies = dataLoaded(data)
+  console.log(filteredMovies);
+})*/
+
+
+/* Oef 4 oefeningen
+onMounted(async () => {
+  const data = await d3.csv('abonnees_genk.csv', typeConversion);
+  const filteredCode = dataLoaded(data);
+  const groupedData = groupByPostcode(filteredCode);
+  const filteredGroupedData = filterGroupedData(groupedData);
+  console.log(filteredGroupedData);
+});
+
+function typeConversion(d) {
+  const PostcodeNumber = parseInt(d.Postcode);
+  return {
+    Postcode: PostcodeNumber
+  };
+}
+
+function dataLoaded(d) {
+  return d.filter(r => {
+    return r.Postcode && String(r.Postcode).length === 4;
+  });
+}
+
+function groupByPostcode(data) {
+  return d3.rollup(
+      data,
+      v => v.length,
+      d => d.Postcode
+  );
+}
+
+function filterGroupedData(groupedData) {
+  return Array.from(groupedData, d => ({ Postcode: d[0], AantalAbonnees: d[1] }))
+      .sort((a, b) => b.AantalAbonnees - a.AantalAbonnees);
+}*/
 </script>
 
 <template>
-
+<!-- oefening 3 Lab + oef 1 oefeningen
+  <svg width=700 height=350>
+    <g v-for="(m, index) in movieData">
+      <g :transform="`translate(${index * 55}, ${305 - xScale(m.revenue)})`">
+        <rect :width="20" :height=xScale(m.revenue) fill=orange />
+        <text transform="rotate(10,0,20)" text-anchor="middle" fill="white">{{ m.genre }}</text>
+      </g>
+    </g>
+  </svg>-->
 </template>
 
 <style scoped>
